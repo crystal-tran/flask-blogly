26f518d5f871a1ceb552c0cb6b0a5e1856f95204
Crystal Tran <c.t.tran95@gmail.com>
2023-12-06 18:39:07 -0500

    created User class, and base HTML

--- app.py
+++ app.py
@@ -2,8 +2,9 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -11,4 +12,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+app.config['SECRET_KEY'] = "blogly"
+
 connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
+
+@app.get()
--- models.py
+++ models.py
@@ -1,1 +1,40 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "user"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    first_name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.Text(),
+        default='/static/img/default_profile_image.jpg'
+    )
+
+
+def connect_db(app):
+    """Connect the database to our Flask app."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
